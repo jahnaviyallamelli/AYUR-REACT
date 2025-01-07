c7661663d1c0c9c6a5e339c72e761a2193d52c7a
Jahnavi Yallamelli <jahnaviyallamelli@gmail.com>
2025-01-07 12:32:40 +0530

    commit

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+// Get the root element from the DOM
+const container = document.getElementById("root");
+
+// Create a root and render the application
+const root = createRoot(container);
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
